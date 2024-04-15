22d28b9ffe32021fa2ca217ba365beaf43212e3c
Evandro C fraga <evandrofragac@gmail.com>
2024-04-15 15:42:58 -0300

    Aterações de produtos usando massa de dados

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,20 +1,65 @@
 ///<reference type="cypress"/>
 
+import produtosPage from "../../support/page-objects/produtos.page";
+
 
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     });
-    it('Deve selecionar um produto da lista', () => {
-        cy.get('.product-block')
-        //.first() primeiro elemento da lista
-       // .last() trás o ultimo da lista
-      // .eq(2) pega a posição que eu escolher, lembrandro que se começa do zero que e o 1
-        .contains('Apollo Running Short')
-        .click()
 
+    it('Deve selecionar um produto da lista', () => {
+
+        produtosPage.buscarProdutoLista('Aero Daily Fitness Tee')
         cy.get('#tab-title-description > a').should('contain', 'Descrição')
         
     });
+
+    it('Deve buscar um produto com sucesso', () => {
+        let produto = 'Aether Gym Pant'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain' , produto)
+        
+    });
+
+    it('Deve visitar a página do produto', () => {
+        produtosPage.visitarProduto( 'Aether Gym Pant')
+        cy.get('.product_title').should('contain' , 'Aether Gym Pant')
+    });
+
+    it('Deve adicionar um produto ao carrinho', () => {
+        let qtd = 7
+        produtosPage.buscarProduto('Stellar Solar Jacket')
+        produtosPage.addProdutoCarrinho('M','Red', qtd)
+        cy.get('.woocommerce-message').should('contain', qtd + ' × “Stellar Solar Jacket” foram adicionados no seu carrinho')
+        
+    });
+
+    
+        it.only('Deve adicionar um produto ao carrinho buscando da massa de dados', () => {
+            cy.fixture('produtos').then(dados => {
+                produtosPage.buscarProduto(dados[1].nomeProduto)
+                produtosPage.addProdutoCarrinho(
+                    dados[1].tamanho, 
+                    dados[1].cor, 
+                    dados[1].quantidade)
+                cy.get('.woocommerce-message').should('contain', dados[1].nomeProduto)
+
+            })
+        
+            
+        });
+       
+        
+
+
+
+        
+        
+
+
+    
+
+
 });
