cafcb3e82740cf51e0bb332ff2dc73c9e65c1cd6
Evandro C fraga <evandrofragac@gmail.com>
2024-04-22 10:33:02 -0300

    mudança no gitignore

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId:'musds4',
+  
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    setupNodeEvents(on, _config) {
+      
+     //  implement node event listeners here
     },
-    baseUrl: 'http://lojaebac.ebaconline.art.br/'
+    baseUrl: 'http://lojaebac.ebaconline.art.br/',
+    video: true,
   },
 });
